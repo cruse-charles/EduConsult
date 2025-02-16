19d64d093fa4beb568a55ef11db537ca444418de
Charles Cruse <charle9@vt.edu>
2025-02-16 20:05:05 +0800

    added google signin

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -1,12 +1,13 @@
 'use client'
 
 import { useState } from "react"
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth"
+import { getAuth, signInWithEmailAndPassword, GoogleAuthProvider, signInWithPopup } from "firebase/auth"
 import { app } from "@/lib/firebaseConfig"
 
 const page = () => {
-    // Initialize Firebase Auth instance using the configured app
+    // Initialize Firebase Auth instance using the configured app and Google Auth provider
     let auth = getAuth(app);
+    let googleProvider = new GoogleAuthProvider();
 
     // State to manage form input data for email and password
     const [userData, setuserData] = useState({
@@ -19,9 +20,23 @@ const page = () => {
         e.preventDefault()
         
         // Firebase function to create a new user with email and password
-        createUserWithEmailAndPassword(auth, userData.email, userData.password)
+        signInWithEmailAndPassword(auth, userData.email, userData.password)
             .then((userCredential) => {
                 console.log(userCredential)
+            })
+            .catch((error) => {
+                console.group(error.message)
+            })
+    }
+
+    // Handle Google sign-in
+    const handleGoogleSignIn = (e: React.MouseEvent<HTMLButtonElement>) => {
+        e.preventDefault()
+        
+        // Firebase function to sign in with Google
+        signInWithPopup(auth, googleProvider)
+            .then((result) => {
+                console.log(result)
             })
             .catch((error) => {
                 console.group(error.message)
@@ -43,7 +58,8 @@ const page = () => {
     <form onSubmit={handleSubmit}>
         <input onChange={handleInputChange} value={userData.email} name='email' placeholder='email' className='border border-black'/>
         <input onChange={handleInputChange} value={userData.password} name='password' placeholder='password' className='border border-black'/>
-        <button type='submit' className='border border-black'>Login</button>
+        <button type='submit' className='border border-black'>Sign in</button>
+        <button className='border border-black' onClick={handleGoogleSignIn}>Google</button>
     </form>
   )
 }
