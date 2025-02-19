a94d1b09d9d45e7989f6dca92decebf4b2545e97
Charles Cruse <charle9@vt.edu>
2025-02-19 11:25:34 +0800

    added comments and typescript to student profile page

--- app/studentprofile/[id]/page.tsx
+++ app/studentprofile/[id]/page.tsx
@@ -6,38 +6,54 @@ import { useParams } from "next/navigation";
 import { useEffect, useState } from "react";
 import { ref, uploadBytesResumable, uploadBytes  } from "firebase/storage";
 
-function page({params} : {params: {id: string}}) {
+interface Student {
+    name: string;
+    gpa: number;
+}
 
-    const {id} = useParams();
-    const [studentData, setStudentData] = useState(null);
+function page() {
+    // retrieve the student ID from URL and create state to hold student data
+    const { id } = useParams<{id: string}>();
+    const [studentData, setStudentData] = useState<Student | null>(null);
 
-
+    // fetch student data from Firestore when the component mounts and set it to state
     useEffect(() => {
-
         const fetchStudentData = async () => {
             const docRef = doc(db, "studentUsers", id);
             const docSnap = await getDoc(docRef);
             // console.log(docSnap.data())
-            setStudentData(docSnap.data())
+            setStudentData(docSnap.data() as Student | null);
         }
 
         fetchStudentData()
-    })
+    }, [])
 
     // TODO: add monitor uploading process, use these links: 
         // https://firebase.google.com/docs/storage/web/upload-files
         // https://www.youtube.com/watch?v=fgdpvwEWJ9M start at around 30:00
 
-    const handleFileUpload = (event) => {
+    // handle file upload, upload each file to Firebase Storage
+    const handleFileUpload = async (event: React.ChangeEvent<HTMLInputElement>) => {
         const files = event?.target.files
         console.log(files);
 
-        Array.from(files).forEach((file) => {
+        if (!files) {
+            console.error('No files selected');
+            return;
+        }
+
+        for (const file of Array.from(files) as File[]) {
+            // Create a storage reference
             const storageRef = ref(storage, `${file.name}`);
-            uploadBytes(storageRef, file).then((snapshot) => {
-                console.log('Uploaded a blob or file!');
-            });
-        })
+            
+            // Upload the file to the storage reference
+            try {
+                const snapshot = uploadBytes(storageRef, file)
+                console.log('Uploaded a blob or file!', snapshot);
+            } catch (error) {
+                console.error('Error uploading file:', error);
+            }
+        }
     }
 
     return (
