12a8f4b61c0354d20ef34e41ba5e55020c7af13a
Charles Cruse <charle9@vt.edu>
2025-03-07 17:02:23 +0800

    code clean up and added typing

--- lib/assignmentUtils.ts
+++ lib/assignmentUtils.ts
@@ -1,6 +1,6 @@
 import { getDownloadURL, ref, uploadBytes, uploadBytesResumable } from "firebase/storage";
 import { db, storage } from "@/lib/firebaseConfig";
-import { addDoc, arrayUnion, collection, doc, setDoc, updateDoc } from "firebase/firestore";
+import { arrayUnion, doc, setDoc, updateDoc } from "firebase/firestore";
 import { Assignment } from "./types/types";
 import { User } from "firebase/auth";
 import { nanoid } from "@reduxjs/toolkit";
@@ -37,42 +37,6 @@ export const fileUpload = async (files: File[], studentId: string) => {
 
     return filesData
 }
-
-
-// export const uploadAssignment = async (assignmentData: Assignment, assignmentsDocId: string | undefined, studentId: string, consultant: User | null) => {
-//     try {
-//         let newAssignmentsDocId = assignmentsDocId;
-    
-//         // If we have assignments already and a ref to it for the student, just update this doc
-//         if (assignmentsDocId) {
-//             const assignmentsDocRef = doc(db, 'assignments', assignmentsDocId);
-//             await updateDoc(assignmentsDocRef, {
-//                 assignments: arrayUnion(assignmentData)
-//             }) 
-//         } else {
-//             // If we don't have any previous assignments and no ref, create a new Doc
-//             const assignmentDocRef = await addDoc(collection(db, "assignments"), {
-//                 student: studentId,
-//                 consultant: consultant?.uid,
-//                 assignments: assignmentData
-//             })
-    
-//             // Create an assignment doc id with the new assignment doc 
-//             newAssignmentsDocId = assignmentDocRef.id
-//         }
-    
-//         // Update folder names in student's doc
-//         await updateDoc(doc(db, "studentUsers", studentId), {
-//             assignmentsDocId: newAssignmentsDocId,
-//             folders: arrayUnion(assignmentData.folderName)
-//         })
-    
-//     } catch (error) {
-//         console.log("Error adding assignment: ", error)
-//     }
-// }
-
-
 
 export const uploadAssignment = async (assignmentData: Assignment, assignmentsDocId: string | undefined, studentId: string, consultant: User | null) => {
     try {
