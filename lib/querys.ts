01ae55d5b1124be9f897bce7d9aacc891d85a2d8
Charles Cruse <charle9@vt.edu>
2025-04-15 16:16:48 +0800

    code cleanup and comments

--- lib/querys.ts
+++ lib/querys.ts
@@ -4,13 +4,17 @@ import { query, where, getDocs, collection, doc } from 'firebase/firestore';
 import { db } from "@/lib/firebaseConfig";
 import { Timestamp } from "firebase/firestore";
 
+// Function to count tasks due this week for a consultant
 export const countTasksDueThisWeek = async (consultantId: string) => {
 
+    // Get consultant's doc reference
     const consultantRef = doc(db, "consultantUsers", consultantId);
 
+    // Find the start and end of current week
     const start = startOfWeek(new Date())
     const end = endOfWeek(new Date())
 
+    // Count assignments with dueDates within current week
     const q = query(
         collection(db, 'assignments'),
         where('consultant', '==', consultantRef),
@@ -22,6 +26,7 @@ export const countTasksDueThisWeek = async (consultantId: string) => {
     return snapshot.size;
 }
 
+// Function to count students in progress (has pending assighnemnts) for a consultant
 export const countOfInProgressStudents = async (consultantId: string) => {
     const consultantRef = doc(db, "consultantUsers", consultantId);
 
@@ -32,6 +37,5 @@ export const countOfInProgressStudents = async (consultantId: string) => {
     )
 
     const snapshot = await getDocs(q);
-    console.log("Count of in-progress students:", snapshot.size);
     return snapshot.size;
 }
--- lib/statsUtils.ts
+++ lib/statsUtils.ts
@@ -1,7 +1,7 @@
 import { doc, getDoc, increment, Timestamp, updateDoc } from "firebase/firestore";
 import { db } from "./firebaseConfig";
 
-
+// Function to update the next deadline for a student
 export const updateNextDeadline = async (studentId: string, dueDate: Date | Timestamp | undefined) => {
     try {
 
@@ -10,6 +10,7 @@ export const updateNextDeadline = async (studentId: string, dueDate: Date | Time
             return
         }
 
+        // Get the student's doc reference and data
         const studentDocRef = doc(db, "studentUsers", studentId);
         const studentDocSnap = await getDoc(studentDocRef);
 
@@ -37,35 +38,20 @@ export const updateNextDeadline = async (studentId: string, dueDate: Date | Time
     }
 }
 
+// Function to update the pending assignments count for a student
 export const updatePendingCount = async (studentId: string, status: string) => {
     try {
-        // Get the student's doc reference and fetch snapshot
+        // Get the student's doc reference
         const studentDocRef = doc(db, "studentUsers", studentId);
-        // const studentDocSnap = await getDoc(studentDocRef);
-
-        // Check if document exists
-        // if (!studentDocSnap.exists()) {
-        //     console.log("No student document found!");
-        //     return;
-        // }
-
-        // Retrieve student's data and pending assignments count
-        // const studentData = studentDocSnap.data();
-        // const pendingAssignmentsCount = studentData.stats?.pendingAssignmentsCount || 0;
-        // console.log(pendingAssignmentsCount, 'Pending Assignment Count')
 
         // Update the pending assignments count, if added assignment is pending then add to count, if not then subtract
         // Use increment to update the count without reads
         if (status === 'Pending') {
-            console.log('Entering pending clause')
             await updateDoc(studentDocRef, {
-                // "stats.pendingAssignmentsCount": pendingAssignmentsCount + 1
                 "stats.pendingAssignmentsCount": increment(1)
             })
         } else {
-            console.log('Entering other clause')
             await updateDoc(studentDocRef, {
-                // "stats.pendingAssignmentsCount": Math.max(pendingAssignmentsCount - 1, 0)
                 "stats.pendingAssignmentsCount": increment(-1)
             })
         }
