fc556046af6c719f91ef91bc5d94335bc7930194
Charles Cruse <charle9@vt.edu>
2025-05-08 23:04:06 +0800

    adjusted status badge

--- app/assignments/page.tsx
+++ app/assignments/page.tsx
@@ -6,7 +6,8 @@ import { getConsultantCalendarAssignments } from '@/lib/querys'
 import { Assignment } from '@/lib/types/types'
 import { formatNextDeadline } from '@/lib/utils'
 import { RootState } from '@/redux/store'
-import { BookOpen, Check, Clock, Download, FileText, Upload } from 'lucide-react'
+import { Timestamp } from 'firebase/firestore'
+import { BookOpen, Check, CheckCircle, Clock, Download, Eye, FileText, Hourglass, Upload } from 'lucide-react'
 import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
 
@@ -18,6 +19,56 @@ const page = () => {
     useEffect(() => {
         getConsultantCalendarAssignments(user.id).then(setAssignments)
     }, [])
+
+    // TODO: This is in AssignmentsList as well, export it
+        const getStatusBadge = (status: string, dueDate: Date | Timestamp | undefined) => {
+            if (!dueDate) return null
+    
+            if (status === 'Pending' && dueDate < Timestamp.fromDate(new Date())) {
+                return (
+                    <Badge className="gap-1 bg-red-100 text-red-800 font-bold hover:bg-red-100 hover:text-red-800">
+                        <Clock className="h-3 w-3" />
+                        Overdue
+                    </Badge>
+                )
+            }
+    
+            switch (status) {
+                case "Pending":
+                return (
+                    <Badge className="gap-1  bg-orange-100 text-orange-800 font-bold hover:bg-orange-100 hover:text-orange-800">
+                        <Hourglass className="h-3 w-3" />
+                        Assigned
+                    </Badge>
+                )
+    
+                case "Completed":
+                return (
+                    <Badge className="gap-1 bg-green-100 text-green-800 font-bold hover:bg-green-100 hover:text-green-800">
+                        <CheckCircle className="h-3 w-3" />
+                        Completed
+                    </Badge>
+                )
+    
+                case "Submitted":
+                return (
+                    <Badge className="gap-1 bg-blue-100 text-blue-800 font-bold hover:bg-blue-100 hover:text-blue-800">
+                        <Upload className="h-3 w-3" />
+                        Submitted
+                    </Badge>
+                )
+    
+                case "Under Review":
+                return (
+                    <Badge className="gap-1 bg-purple-100 text-purple-800 font-bold hover:bg-purple-100 hover:text-purple-800">
+                        <Eye className="h-3 w-3" />
+                        Reviewing
+                    </Badge>
+                )
+            }
+    
+            return null
+        }
 
 
     return (
@@ -53,19 +104,10 @@ const page = () => {
                                 </div>
                             </TableCell>
                             <TableCell>
-                                <Badge variant={assignment.status === "completed" ? "success" : "outline"}>
-                                {assignment.status === "completed" ? (
-                                    <div className="flex items-center gap-1">
-                                    <Check className="h-3 w-3" />
-                                    Completed
-                                    </div>
-                                ) : (
-                                    <div className="flex items-center gap-1">
-                                    <Clock className="h-3 w-3" />
-                                    Pending
-                                    </div>
-                                )}
-                                </Badge>
+                                {/* <Badge variant={assignment.status === "completed" ? "success" : "outline"}> */}
+                                {getStatusBadge(assignment.status, assignment?.dueDate)}
+                                {/* {assignment.status} */}
+                                {/* </Badge> */}
                             </TableCell>
                             <TableCell>{formatNextDeadline(assignment.dueDate)}</TableCell>
                             </TableRow>
