28fa951087a9bba69ce4c1b5c531e9e449dddefc
cruse-charles <charle9@vt.edu>
2025-10-09 17:45:43 +0800

    added addCases for pending, fulfilled, rejected

--- redux/slices/currentStudentSlice.ts
+++ redux/slices/currentStudentSlice.ts
@@ -128,11 +128,19 @@ const studentSlice = createSlice({
     }
   },
   extraReducers: (builder) => {
-    builder.addCase(fetchStudent.fulfilled, (state, action) => {
-      state.data = action.payload;
-      state.loading = false;
-      state.error = null;
-    });
+    builder
+      .addCase(fetchStudent.pending, (state) => {
+          state.loading = true;
+          state.error = null;
+      })
+      .addCase(fetchStudent.fulfilled, (state, action) => {
+          state.loading = false;
+          state.data = action.payload;
+      })
+      .addCase(fetchStudent.rejected, (state, action) => {
+          state.loading = false;
+          state.error = action.error.message || 'Failed to fetch student';
+      });
   },
 });
 
